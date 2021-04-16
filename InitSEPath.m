90f877cf4d21afdbb4e03f488306d8684426528a
yluATlzu <ylu@lzu.edu.cn>
2021-04-16 20:19:58 +0800

    removed InitSEPath.m

--- InitSEPath.m
+++
@@ -1,9 +0,0 @@
-% Adding the path to the code in this directory
-%
-% Usage
-%    initpath();
-%
-
-[pathstr, ~, ~] = fileparts(mfilename('fullpath'));
-
-addpath(genpath(fullfile(pathstr,'Code')));
